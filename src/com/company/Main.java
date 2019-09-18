9f8f5be82e1a9ff18bf2a133014bcfcd020bc82d
vanderson.nery <vanderson.nery@edu.pucrs.br>
2019-09-18 19:41:02 -0300

    Alterações

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,33 +12,55 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        try {
-            long inTemp = System.currentTimeMillis();
+        File[] arquive = new File[8];
+        arquive[0] = new File(Main.class.getResource(Main.class.getSimpleName()+".class"+"/../teste/casom3.txt").getPath());
+        arquive[1] = new File(Main.class.getResource(Main.class.getSimpleName()+".class"+"/../teste/casom5.txt").getPath());
+        arquive[2] = new File(Main.class.getResource(Main.class.getSimpleName()+".class"+"/../teste/casom7.txt").getPath());
+        arquive[3] = new File(Main.class.getResource(Main.class.getSimpleName()+".class"+"/../teste/casom9.txt").getPath());
+        arquive[4] = new File(Main.class.getResource(Main.class.getSimpleName()+".class"+"/../teste/casom10.txt").getPath());
+        arquive[5] = new File(Main.class.getResource(Main.class.getSimpleName()+".class"+"/../teste/casom11.txt").getPath());
+        arquive[6] = new File(Main.class.getResource(Main.class.getSimpleName()+".class"+"/../teste/casom12.txt").getPath());
+        arquive[7] = new File(Main.class.getResource(Main.class.getSimpleName()+".class"+"/../teste/casom13.txt").getPath());
 
-            FileReader arq = new FileReader("H:\\Semestre4\\alest2\\T1\\arquivo_teste\\casom3.txt");
-            BufferedReader lerArq = new BufferedReader(arq);
+        for (int i=0; i<8; i++) {
+            long somaParciais = 0;
 
-            String linha = lerArq.readLine();
+            System.out.printf("\nExecutando com arquivo %s", arquive[i].getName());
 
-            String[] nLinha = linha.split(" ");
+            for (int j=0; j<10; j++) {
+                long inTemp = System.currentTimeMillis();
 
-            ArrayList<String> nLista = new ArrayList<>();
+                try {
+                    FileReader arq = new FileReader(arquive[i]);
+                    BufferedReader lerArq = new BufferedReader(arq);
 
-            for (String n: nLinha) {
-                nLista.add(n);
+                    String linha = lerArq.readLine();
+
+                    String[] nLinha = linha.split(" ");
+
+                    ArrayList<String> nLista = new ArrayList<>();
+
+                    for (String n : nLinha) {
+                        nLista.add(n);
+                    }
+
+                    nNodo = "1";
+
+                    XTree raiz = new XTree(nNodo, nLista);
+
+                    //raiz.printNodo();
+
+                    long outTemp = System.currentTimeMillis();
+
+                    System.out.printf("\n\tIteração: %d\n\tAltura maxima: %d\n\tSomaTotal: %d\n\tTimer Execute: %.3f m/s\n", j, raiz.getAlturaMaxima(), raiz.getSomaTotal(), ((outTemp - inTemp) / 1000d));
+
+                    somaParciais += (outTemp - inTemp);
+                    arq.close();
+                } catch (IOException e) {
+                    System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+                }
             }
-
-            nNodo = "1";
-
-            XTree raiz = new XTree(nNodo, nLista);
-
-            //raiz.printNodo();
-
-            System.out.printf("Altura maxima: %d\nSomaTotal: %d\nTimer: %.3f m/s\n",raiz.getAlturaMaxima(),raiz.getSomaTotal(),((System.currentTimeMillis() - inTemp)/1000d));
-
-            arq.close();
-        } catch (IOException e) {
-            System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+            System.out.printf("\nTempo Médio: %.3f m/s\n", ((somaParciais / 10) / 1000d));
         }
     }
 }
